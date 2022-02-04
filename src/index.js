812c01f53a0c6c1e7f1441a97b31f9eb3d7bc566
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-02-04 23:11:48 +0100

    theme, ThemeProvider added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GlobalStyle } from './globalStyles';
+import { ThemeProvider } from 'styled-components';
+import { themeLight } from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={themeLight}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/theme.js
@@ -0,0 +1,24 @@
+const colorNames = {
+  white: "#FFFFFF",
+  whiteLilac: "#FBFBFE",
+  mercury: "#E5E5E5",
+  mineShaft: "#252525",
+  scienceBlue: "#0366D6",
+  slateGrey: "#6E7E91",
+
+};
+
+export const themeLight = {
+  colors: {
+    primary: colorNames.scienceBlue,
+    textPrimary: colorNames.mineShaft,
+    site: {
+      background: colorNames.whiteLilac,
+      text: colorNames.slateGrey,
+    },
+    boxBackground: colorNames.white,
+    titleUnderline: colorNames.mercury,
+
+  }
+
+};
