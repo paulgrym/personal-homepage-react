43d435f386b3def9e0cf15f2311fdd3eec613a2f
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-02-09 00:07:44 +0100

    media queries added to Section

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -6,6 +6,11 @@ export const StyledSection = styled.section`
   background:${({ theme }) => theme.colors.boxBackground};
   box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
   border-radius: 4px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.small}px){
+    padding:16px;
+    margin-bottom:48px;
+  }
 `;
 
 export const Title = styled.h2`
@@ -13,9 +18,13 @@ export const Title = styled.h2`
   font-size:30px;
   line-height: 36px;
   font-weight:900;
-  letter-spacing: 0.05em;
   padding-bottom:15px;
   border-bottom: 1px solid ${({ theme }) => theme.colors.titleUnderline};
   color:${({ theme }) => theme.colors.textPrimary};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.small}px){
+    font-size:18px;
+    line-height: 22px;
+    padding-bottom:12px;
 `;
 
--- src/features/homePage/Image/index.js
+++ src/features/homePage/Image/index.js
@@ -2,4 +2,8 @@ import styled from "styled-components";
 
 export const Image = styled.img`
   margin-left:16px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.small}px){
+    margin-left:12px;
+    width:18px;
 `
