ff5df0ba1d0270640b39e1614963c3f66f8e8857
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-02-08 16:47:53 +0100

    Section styles updated

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -5,14 +5,16 @@ export const StyledSection = styled.section`
   margin-bottom:72px;
   background:${({ theme }) => theme.colors.boxBackground};
   box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
+  border-radius: 4px;
 `;
 
 export const Title = styled.h2`
   margin:0;
   font-size:30px;
+  line-height: 36px;
   font-weight:900;
   letter-spacing: 0.05em;
-  padding-bottom:18px;
+  padding-bottom:15px;
   border-bottom: 1px solid ${({ theme }) => theme.colors.titleUnderline};
   color:${({ theme }) => theme.colors.textPrimary};
 `;
