b2a5d13ef83d1c3d15351fcb8d34b9633c8cdc77
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-02-08 16:46:17 +0100

    dark theme colors and breakpoints added

--- src/theme.js
+++ src/theme.js
@@ -1,14 +1,24 @@
 const colorNames = {
   white: "#FFFFFF",
   whiteLilac: "#FBFBFE",
+  ironLight: "rgba(209, 213, 218, 0.3)",
   mercury: "#E5E5E5",
   mineShaft: "#252525",
+  mineShaftLight: "rgba(54,54,54,0.72)",
+  dodgerBlue: "#2188FF",
   scienceBlue: "#0366D6",
   slateGrey: "#6E7E91",
 
 };
 
+const common = {
+  small: 767,
+  medium: 991,
+  large: 1199,
+}
+
 export const themeLight = {
+  ...common,
   colors: {
     primary: colorNames.scienceBlue,
     textPrimary: colorNames.mineShaft,
@@ -17,7 +27,22 @@ export const themeLight = {
       text: colorNames.slateGrey,
     },
     boxBackground: colorNames.white,
-    titleUnderline: colorNames.mercury,
+    titleUnderline: colorNames.ironLight,
+
+  }
+};
+
+export const themeDark = {
+  ...common,
+  colors: {
+    primary: colorNames.dodgerBlue,
+    textPrimary: colorNames.white,
+    site: {
+      background: colorNames.mineShaft,
+      text: colorNames.white,
+    },
+    boxBackground: colorNames.mineShaftLight,
+    titleUnderline: colorNames.ironLight,
 
   }
 
