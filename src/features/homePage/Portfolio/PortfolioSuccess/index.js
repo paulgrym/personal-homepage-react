e23ef54b3a45910fdea7ea4f0a5b2bd7021135ce
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-03-24 22:03:04 +0100

    portfolio filtered and sorted by create date

--- src/features/homePage/Portfolio/PortfolioSuccess/index.js
+++ src/features/homePage/Portfolio/PortfolioSuccess/index.js
@@ -1,14 +1,27 @@
 import Tile from "../Tile";
 import { StyledPortfolio } from "./styled"
 
-const PortfolioSuccess = () => {
+const PortfolioSuccess = ({ portfolio }) => {
+  const filteredPortfolio = portfolio.filter(({ name }) => (
+    name !== "paulgrym"
+    && name !== "personal-homepage-react"
+  ));
+
+  const sortedByCreateDayPortfolio = filteredPortfolio.sort((a, b) => {
+    return new Date(b.created_at) - new Date(a.created_at);
+  });
 
   return (
     <StyledPortfolio>
-      <Tile title={"Test 1"} description={"Test 1"} demoUrl="Test 1" repoUrl="Test 1" />
-      <Tile title={"Test 2"} description={"Test 2"} demoUrl="Test 2" repoUrl="Test 2" />
-      <Tile title={"Test 3"} description={"Test 3"} demoUrl="Test 3" repoUrl="Test 3" />
-      <Tile title={"Test 4"} description={"Test 4"} demoUrl="Test 4" repoUrl="Test 4" />
+      {sortedByCreateDayPortfolio.map(repo => (
+        <Tile
+          key={repo.id}
+          title={repo.name}
+          description={repo.description}
+          demoUrl={repo.homepage}
+          repoUrl={repo.html_url}
+        />
+      ))}
     </StyledPortfolio>
   )
 };
