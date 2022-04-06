a1e804af219aff7c6232f056679d6304d7f34c49
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-04-06 15:51:58 +0200

    word wrapping added to global styles

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -16,6 +16,7 @@ export const GlobalStyle = createGlobalStyle`
     font-family: 'Inter', sans-serif;
     letter-spacing: 0.05em;
     transition: 0.5s ease;
+    overflow-wrap: anywhere;
 
     @media (max-width:${({ theme }) => theme.breakpoints.small}px){
       margin: 20px 16px 28px;
