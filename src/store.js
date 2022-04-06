18ce0e0c5c570001fa2b7d6b25656dcedc7fbd1b
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-04-06 22:31:49 +0200

    Theme added to store

--- src/store.js
+++ src/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from "redux-saga";
 import rootSaga from './rootSaga';
 import { portfolioReducer } from './features/homePage/Portfolio/portfolioSlice';
+import { themeReducer } from './common/ThemeSwitcher/themeSlice';
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
     portfolio: portfolioReducer,
+    theme: themeReducer,
   },
   middleware: [sagaMiddleware],
 });
