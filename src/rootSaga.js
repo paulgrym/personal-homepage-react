5d449a4f651481afabb066e4b7ef35f78c89ec6c
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-04-06 22:30:58 +0200

    Theme saga added to rootSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,9 +1,11 @@
 import { all } from "redux-saga/effects";
+import { watchSaveThemeInLocalStorage } from "./common/ThemeSwitcher/themeSaga";
 import { watchFetchPortfolio } from "./features/homePage/Portfolio/portfolioSaga";
 
 
 export default function* rootSaga() {
   yield all([
     watchFetchPortfolio(),
+    watchSaveThemeInLocalStorage(),
   ]);
 };
